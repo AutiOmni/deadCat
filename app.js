1e47e1a29c740db431dad4fbb4aaf5f70850ee4d
Austin Olijar <73905380+AutiOmni@users.noreply.github.com>
2021-06-22 12:15:02 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let tickers = []
 let myStocksNyse = []
 let myStocksNas = []
 
+regex = /A-Z/g
 
 let j = 0
 let nyseHolder = []
@@ -12,7 +13,11 @@ let nasdaqHolder = []
 let combinedStockDrop = []
 
 let tickersLength = 23485
-async function apr() {
+
+async function tradableSymbols() {
+
+    try {
+
     const res = await fetch('https://financialmodelingprep.com/api/v3/available-traded/list?apikey=4d4593bc9e6bc106ee9d1cbd6400b218')
     const data = await res.json()
     tickers.push(data)
@@ -23,165 +28,83 @@ async function apr() {
             myStocksNas.push(tickers[0][i].symbol)
         }
     }
-  console.log(myStocksNyse, myStocksNas)
-}
+    console.log(myStocksNyse, myStocksNas)
 
-apr()
+  } catch(e) {
 
-/*
-async function getNyseB() {
-   
-    const res = await fetch('http://api.marketstack.com/v1?access_key=639c314e89747310d83acee2912614dc');
-    const data = await res.json();
-    console.log(data)
-    
-}
-
-
-
-    getNyseB() 
-
-*/
-/*
-
-derpy()
-
-function createFeed() {
-
-fetch(url)
-.then(res => res.json())
-.then(data => {
-
-  
-
-    for (let i = 0; i < 5; i++) {
-
-
-    const {ticker, changesPercentage, price, changes} = data[i]
-
-        let urlTwo = `https://financialmodelingprep.com/api/v3/quote/${ticker}?apikey=4d4593bc9e6bc106ee9d1cbd6400b218`
-
-        fetch(urlTwo)
-            .then(res => res.json())
-            .then(dataTwo => {
-                
-                const {avgVolume, volume} = dataTwo[0]
-
-                
-
-                const volIncrease = (volume - avgVolume) / avgVolume * 100
-
-                var volumeChange = volIncrease.toFixed(0)
-                
-                const nerpy = document.createElement('div')
-                nerpy.classList.add('text-center')
-                nerpy.classList.add('col', 'border', 'p-2')
-                nerpy.innerHTML = `<h3 id="current-price">Volume Change:${volumeChange}%</h3>`
-                rowTwo.appendChild(nerpy)
-                
-            })
-            
-
-
-   /* THIS CREATES THE DIV     
-
-    const derpy = document.createElement('div')
-    derpy.classList.add('text-center')
-    derpy.classList.add('col', 'border', 'p-2')
-    derpy.innerHTML = `<h2 id="ticker">${ticker}</h2>
-    <h3 id="percentage-down">Down: ${changesPercentage}</h3>
-    <h3 id="share-price-change">Share Price Change:$${changes}</h3>
-    <h3 id="current-price">Current Price: $${price}</h3>`
-    rowOne.appendChild(derpy)
-    }
-
-}).catch(error => {
-    console.log(('Error Depr'), error)
-})
- 
-
+    console.log(e)
+ }
 
 }
-createFeed() 
-
-*/ 
-
-
-function generateStocks() {
-
 
 
 /* ---------------------- NYSE DROP ------------------------- */
 
-
 async function getNyseBottom() {
+    try {
 
     const res = await fetch('https://financialmodelingprep.com/api/v3/quotes/nyse?apikey=4d4593bc9e6bc106ee9d1cbd6400b218')
     const data = await res.json()
 
-    function pushItNye() {
-
     for (let i = 0; i < data.length; i++) {
-        if (data[i].changesPercentage < -10 && myStocksNyse.indexOf(data[i].symbol) > 0) {
+        if (data[i].changesPercentage < -5 && myStocksNyse.indexOf(data[i].symbol) > 0) {
       nyseHolder.push(data[i])
         }
     }
     console.log(nyseHolder)
+    } catch(e) {
+        console.log(e)
+    }
+  
 }
 
-pushItNye()
-}
-
-getNyseBottom()
 
 /* ---------------------- NASDAQ DROP ------------------------- */
 
 async function getNasdaqBottom() {
-
+    try {
     const res = await fetch('https://financialmodelingprep.com/api/v3/quotes/nasdaq?apikey=4d4593bc9e6bc106ee9d1cbd6400b218')
     const data = await res.json()
 
-    function pushItNas() {
-
     for (let i = 0; i < data.length; i++) {
-        if(data[i].changesPercentage < -10 && myStocksNas.indexOf(data[i].symbol) > 0) {
+        if(data[i].changesPercentage < -5 && myStocksNas.indexOf(data[i].symbol) > 0) {
       nasdaqHolder.push(data[i])
         }
     }
-console.log(nasdaqHolder)
+    console.log(nasdaqHolder)
+    } catch(e) {
+        console.log(e)
+    }
 }
 
-pushItNas()
-}
-
-getNasdaqBottom()
-
-
-
-}
-
-setTimeout(generateStocks, 2500)
-
-
-
-function makeIt() {
 
 
 /* ---------------------- COMBINE AND SORT LARGEST DROP ------------------------- */
+function compileStocks() {
 
 rowOne.innerHTML = ''
 
-combinedStockDrop = combinedStockDrop.concat(nasdaqHolder, nyseHolder)
+/* ------- THIS IS A FILTER FOR WEIRD STOCK SYMBOLS THAT SLIP IN ----------*/ 
 
-for (let i = 0; i < combinedStockDrop.length; i++) {
-combinedStockDrop.sort((a,b) => {
+
+combinedStockDrop = combinedStockDrop.concat(nasdaqHolder, nyseHolder)
+console.log(combinedStockDrop)
+
+const keys = /[\W\d]/g
+finalChart = combinedStockDrop.filter(stock => {
+    return !stock.symbol.match(keys)
+})
+console.log(finalChart)
+
+for (let i = 0; i < finalChart.length; i++) {
+finalChart.sort((a,b) => {
     return a.changesPercentage - b.changesPercentage
     })
 }
 
 while (j < 4) {
 
-    const {symbol, changesPercentage, price, change} = combinedStockDrop[j]
+    const {symbol, changesPercentage, price, change} = finalChart[j]
 
     changesPercentagePositive = changesPercentage * -1
 
@@ -201,11 +124,24 @@ while (j < 4) {
 
     j++
 
+  }
+ 
 }
-console.log(combinedStockDrop)
+
+/*----- BUILD TO PAGE -----*/
+
+async function buildToPage() {
+    try {
+
+    await tradableSymbols()
+    await getNyseBottom()
+    await getNasdaqBottom()
+   
+    compileStocks()
+
+    } catch {
+        console.log('Build To Page Error')
+    }
 }
 
-
-setTimeout(makeIt, 5000)
-
-/*console.log(combinedStockDrop)*/
+buildToPage()
