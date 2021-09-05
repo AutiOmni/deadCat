3d69af262918e342338a02cf6c5e5ce21c7a5947
Austin Olijar <73905380+AutiOmni@users.noreply.github.com>
2021-09-05 13:12:36 -0500

    Add files via upload

--- animation.js
+++ animation.js
@@ -37,22 +37,22 @@ $(window).scroll(function() {
         $('.info-cont-one').addClass('active-info-one')
     }
 
-	if ((windowTop * 1.25) > topInfoTwoR)
+	if ((windowTop * 1.5) > topInfoTwoR)
     {
         $('.confused-chart-right').addClass('active-info-two-r')
        
     }
 
-	if ((windowTop * 1.25) > topInfoTwoL)
+	if ((windowTop * 1.5) > topInfoTwoL)
     {
         $('.confused-chart-left').addClass('active-info-two-l')       
     }
 
-	if (windowTop * 1.25 > topInfoThreeR)
+	if (windowTop * 1.5 > topInfoThreeR)
     {
         $('.selector-info-r').addClass('active-info-three')
     }
-	if (windowTop * 1.25 > topInfoThreeL)
+	if (windowTop * 1.5 > topInfoThreeL)
     {
         $('.selector-info-l').addClass('active-info-three')
     }
