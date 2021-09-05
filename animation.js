e264053a78f9deaf3926b1342f32c9b6a089789a
Austin Olijar <73905380+AutiOmni@users.noreply.github.com>
2021-09-05 12:44:58 -0500

    Add files via upload

--- animation.js
+++ animation.js
@@ -24,24 +24,36 @@ $(window).scroll(function() {
     var windowTop = window.pageYOffset;
     var topInfoOne = $('.info-cont-one').offset().top
 
-    var topInfoTwo = $('.confused-chart').offset().top
+    var topInfoTwoL = $('.confused-chart-left').offset().top
 
-    var topInfoThree = $('.selector-info').offset().top
-console.log(topInfoThree, windowTop)
+    var topInfoTwoR = $('.confused-chart-right').offset().top
+
+    var topInfoThreeR = $('.selector-info-r').offset().top
+    var topInfoThreeL = $('.selector-info-l').offset().top
+
+
 	if ((windowTop * 3) > topInfoOne)
     {
         $('.info-cont-one').addClass('active-info-one')
     }
 
-	if ((windowTop * 1.5) > topInfoTwo)
+	if ((windowTop * 1.25) > topInfoTwoR)
     {
         $('.confused-chart-right').addClass('active-info-two-r')
-        $('.confused-chart-left').addClass('active-info-two-l')
+       
     }
 
-	if (windowTop * 1.25 > topInfoThree)
+	if ((windowTop * 1.25) > topInfoTwoL)
+    {
+        $('.confused-chart-left').addClass('active-info-two-l')       
+    }
+
+	if (windowTop * 1.25 > topInfoThreeR)
     {
         $('.selector-info-r').addClass('active-info-three')
+    }
+	if (windowTop * 1.25 > topInfoThreeL)
+    {
         $('.selector-info-l').addClass('active-info-three')
     }
 
