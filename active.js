7de7e659f0bdb6e3696894b7133b0be8c34dfe6d
Austin Olijar <73905380+AutiOmni@users.noreply.github.com>
2021-09-22 17:42:21 -0500

    Add files via upload

--- active.js
+++ active.js
@@ -1,6 +1,12 @@
 //SEARCH FUNCTION
 $('.search-icon').click(function() {
-    $('.search-bar').toggleClass('active-search')
+
+    $('.search-bar').toggleClass('active-search');
+
+    if ($('.search-bar').hasClass('active-search'))
+    {
+        $(".search-bar input[type=text]").focus();
+    }
 }) 
 
 $('.search-text').on('keyup', function() {
